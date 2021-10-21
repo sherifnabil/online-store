465b16b44f0979e23400432e97e88e1b04a5a4af
Sherif <sherifnabil@github.com>
2021-10-21 02:08:53 +0200

    Moving HasUuid  Trait to Shared Domain

--- src/Domains/Customer/Models/Location.php
+++ src/Domains/Customer/Models/Location.php
@@ -6,7 +6,7 @@ namespace Domains\Customer\Models;
 
 use Database\Factories\LocationFactory;
 use Illuminate\Database\Eloquent\Model;
-use Domains\Customer\Models\Concerns\HasUuid;
+use Domains\Shared\Models\Concerns\HasUuid;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
