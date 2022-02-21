0c981836cef56c3762cd0a54c91499e01c1ae58f
Sherif <sherifnabil@github.com>
2022-02-21 19:11:59 +0200

    cart tests remove item change quantity etc

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -1,11 +1,18 @@
 <?php
 
+use function Pest\Laravel\assertDeleted;
+use function Pest\Laravel\delete;
 use function Pest\Laravel\get;
+use function Pest\Laravel\patch;
 use function Pest\Laravel\post;
 
 use Domains\Catalog\Models\Variant;
+use Domains\Customer\Events\DecreaseCartQuantity;
+use Domains\Customer\Events\IncreaseCartQuantity;
+use Domains\Customer\Events\ProductWasRemovedFromCart;
 use Illuminate\Http\Response;
 use Domains\Customer\Models\Cart;
+use Domains\Customer\Models\CartItem;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Domains\Customer\States\Statuses\CartStatus;
 use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent;
@@ -63,4 +70,87 @@ it('can add a new product to a cart', function () {
     ->assertStatus(
         status: Response::HTTP_CREATED
     );
+});
+
+it('can increase the quantity of an item in the cart', function () {
+    // expect(EloquentStoredEvent::query()->get())->toHaveCount(0);
+
+    $item = CartItem::factory()->create(['quantity' =>  1]);
+
+    expect($item->quantity)->toBe(1);
+
+    patch(
+        uri: route('api:v1:carts:products:update', [
+            'cart'  =>  $item->cart->uuid,
+            'item'  =>  $item->uuid,
+        ]),
+        data: ['quantity' => 3]
+    )
+    ->assertStatus(
+        status: Response::HTTP_ACCEPTED
+    );
+
+    // expect(EloquentStoredEvent::query()->get())->toHaveCount(1);
+    expect(EloquentStoredEvent::query()->orderBy('id', 'desc')->first()->event_class)->toEqual(IncreaseCartQuantity::class);
+});
+
+it('can decrease the quantity of an item in the cart', function () {
+    // expect(EloquentStoredEvent::query()->get())->toHaveCount(0);
+
+    $item = CartItem::factory()->create(['quantity' =>  3]);
+
+    expect($item->quantity)->toBe(3);
+
+    patch(
+        uri: route('api:v1:carts:products:update', [
+            'cart'  =>  $item->cart->uuid,
+            'item'  =>  $item->uuid,
+        ]),
+        data: ['quantity' => 2]
+    )
+    ->assertStatus(
+        status: Response::HTTP_ACCEPTED
+    );
+
+    // expect(EloquentStoredEvent::query()->get())->toHaveCount(1);
+    expect(EloquentStoredEvent::query()->orderBy('id', 'desc')->first()->event_class)->toEqual(DecreaseCartQuantity::class);
+});
+
+it('remove the item from the cart when quantity is zero', function () {
+    $item = CartItem::factory()->create(['quantity' =>  3]);
+
+    expect($item->quantity)->toBe(3);
+
+    patch(
+        uri: route('api:v1:carts:products:update', [
+            'cart'  =>  $item->cart->uuid,
+            'item'  =>  $item->uuid,
+        ]),
+        data: ['quantity' => 0]
+    )
+    ->assertStatus(
+        status: Response::HTTP_ACCEPTED
+    );
+
+    // expect(EloquentStoredEvent::query()->get())->toHaveCount(1);
+    expect(EloquentStoredEvent::query()->orderBy('id', 'desc')->first()->event_class)->toEqual(ProductWasRemovedFromCart::class);
+});
+
+it('can remove an item from the cat', function () {
+    $item = CartItem::factory()->create(['quantity' =>  3]);
+
+    delete(
+        uri: route(
+            'api:v1:carts:products:delete',
+            [
+                'cart'  =>  $item->cart->uuid,
+                'item'  =>  $item->uuid,
+            ]
+        )
+    )->assertStatus(
+        status: Response::HTTP_ACCEPTED
+    );
+
+    // assertDeleted($item);
+    expect(EloquentStoredEvent::query()->orderBy('id', 'desc')->first()->event_class)->toEqual(ProductWasRemovedFromCart::class);
 });
